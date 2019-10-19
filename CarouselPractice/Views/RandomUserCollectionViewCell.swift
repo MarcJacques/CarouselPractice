eb2c5454b97006ccd5124eff48aa6ce0120a95aa
Marc Jacques <Marcjacques@me.com>
2019-10-19 15:30:31 -0400

    Organized files into folders

---
+++ CarouselPractice/Views/RandomUserCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RandomUserCollectionViewCell.swift
+//  CarouselPractice
+//
+//  Created by Marc Jacques on 10/19/19.
+//  Copyright © 2019 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserCollectionViewCell: UICollectionViewCell {
+    
+}
