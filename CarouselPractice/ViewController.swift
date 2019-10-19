0ae57bc0b131b54ab5a83ea58ceaa98ef7281137
Marc Jacques <Marcjacques@me.com>
2019-10-19 13:18:04 -0400

    Initial Commit

---
+++ CarouselPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselPractice
+//
+//  Created by Marc Jacques on 10/19/19.
+//  Copyright © 2019 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
